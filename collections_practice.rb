d3d30a2195cc1e9fc0428e9e716a56db183919a5
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-20 09:08:41 +0100

    Add solution for questions 5-9

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def sort_array_asc(array)
   array.sort
 end
@@ -18,4 +19,26 @@ end
 def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map { |x| x.gsub("#{x[2]}", "$") }
+end
+
+def find_a(array)
+  array.select { |x| x.start_with?("a") }
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  new_array = array.each_with_index.collect{|element, index| element.concat("s")}
+  new_array[1].delete!("s")
+  new_array
 end
